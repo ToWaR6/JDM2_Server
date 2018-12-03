4bb1672e1ed394c2e0c946f5f56ca95970cc520a
lcrauser <loic.crauser@free.fr>
2018-12-03 21:05:17 +0100

    add home page

--- src/main/java/Requester/RequestController.java
+++ src/main/java/Requester/RequestController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import requeterRezo.Mot;
 import requeterRezo.RequeterRezoDump;
